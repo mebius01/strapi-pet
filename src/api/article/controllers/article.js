e41d7215bc44566d755dff7995a362f2f7f9b6e3
iv <consmebius@gmail.com>
2022-09-15 08:35:31 +0300

    ...

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -9,6 +9,11 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::article.article', ({ strapi }) => ({
   async create(ctx) {
+    const user = ctx.state.user
+    const userDb = await strapi.db.query('api::article.article').findOne({ id: 1 }).select(['user_id'])
+    console.log(userDb);
+    // let { isp } = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
+    // ctx.state.user.isp = isp;
     let entity;
     entity = await super.create(ctx);
     return entity;
