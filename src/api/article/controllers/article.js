6e3f298cc094e77391add410fc5f99fca7aa6dec
iv <consmebius@gmail.com>
2022-09-16 17:29:21 +0300

    this is fucking bull shit

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,5 +1,4 @@
 'use strict';
-const { sa } = require('@strapi/strapi');
 
 /**
  * article controller
@@ -9,12 +8,10 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::article.article', ({ strapi }) => ({
   async create(ctx) {
-    const user = ctx.state.user
-    const userDb = await strapi.db.query('api::article.article').findOne({ id: 1 }).select(['user_id'])
-    console.log(userDb);
-    // let { isp } = await strapi.query('user', 'users-permissions').findOne({ id: ctx.state.user.id });
-    // ctx.state.user.isp = isp;
     let entity;
+    console.log(ctx.request.body.data);
+    ctx.request.body.data.user = ctx.state.user;
+    console.log(ctx.request.body.data);
     entity = await super.create(ctx);
     return entity;
   }
